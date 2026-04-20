e5e04e505a95b02cda7224c0cfeddbef9593aa69
gabinhallosserie <gabin.hallosserie@beproject.fr>
2026-04-20 12:43:48 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+    import { ref } from 'vue';
+
+    const count = ref(0)
+    const title = "<span>Bienvenue sur le compteur</span>"
+
+    console.log(count, count.value)
+
+    const increment = () => { count.value++}
+    const decrement = () => { count.value--}
+</script>
+
+<template>
+    <h1 v-html="title"></h1>
+
+    <p :style="{ color: count > 5 ?'red' : 'green' }">Compteur : {{ count }}</p>
+
+    <div v-if="count>5">Bravo vous avez cliqué plus de 5 fois</div>
+    <div v-else>Vous avez cliqué moins de 5 fois</div>
+
+    <button @click="decrement">Décrémenter</button>
+    <button @click="increment">Incrémenter</button>
+</template>
+
+<style>
+    
+</style>
